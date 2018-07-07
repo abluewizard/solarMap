e06be176aac9eeea972e71a70021de90da7cfda2
abluewizard <39067251+abluewizard@users.noreply.github.com>
2018-07-07 15:08:50 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ var moonText;
 function preload() {
   // Ensure the .ttf or .otf font stored in the assets directory
   // is loaded before setup() and draw() are called
-  font = loadFont('solarMap/assets/Roboto-Thin.ttf');
+  font = loadFont('/assets/Roboto-Thin.ttf');
 }
 
 function setup() {
